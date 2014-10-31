2793b1072f193e72616460bf2ea7e2196ae1a15b
Aldren Terante <aldren.terante@salarium.com>
2014-10-31 18:55:43 +0800

    Working in adding objects

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var App = angular.module('App', ['firebase']);
 
     App.controller('TodoCtrl', function($scope, $http, $firebase) {
-        var ref = new Firebase("https://brilliant-torch-3226.firebaseio.com/");        
+
+        // initialize firebase api
+        var ref = new Firebase("https://brilliant-torch-3226.firebaseio.com");        
 
         // create an AngularFire reference to the data
         var sync = $firebase(ref);
@@ -9,14 +11,29 @@ var App = angular.module('App', ['firebase']);
         // download the data into a local object
         $scope.todos = sync.$asObject();
 
-        $scope.master = {};
+        // create a object for new todo
+        $scope.todo = {};
 
+        // save the object data from $scope.todo
         $scope.saveTodo = function ( todo ) {
-            $scope.master = angular.copy( todo );            
+
+            // prepare data
+            var data = { "done" : false, "text" : todo.new };            
+                sync.set(data);
+
+            // clear data after saving
+            $scope.reset();
         };
         
+        // clear input box data
         $scope.reset = function() {
-            $scope.todo = angular.copy($scope.master);
-        };        
+            $scope.todo = '';
+        };               
+
+        $scope.completed;
+        $scope.setStatus = function ( index, todo ) {
+            $scope.completed = index;
+            if ( todo.done == true ) { todo.done = false; } else { todo.done = true; }
+        }
 
     });
